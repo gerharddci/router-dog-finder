e21ae4d2b55a46addd995e665b971aa95b0c138b
gnuffer <gfnuffer@gmail.com>
2022-09-27 20:56:26 +0200

    Make minor change to h1

--- src/routes/Dogs.js
+++ src/routes/Dogs.js
@@ -5,7 +5,7 @@ import NavBar from '../NavBar';
 export default function Dogs() {
     return (
         <div >
-            <h1>HELLO! WE HAVE DOGS. CLICK ON THEM FOR MORE INFO!</h1>
+            <h1>HELLO! WE HAVE DOGS. CLICK ON THEM FOR MORE INFO</h1>
             <NavBar />
             <Outlet />
         </div>
